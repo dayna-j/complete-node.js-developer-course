a17bed70581657ad006d66e331e94d5f41dc26d4
Dayna Blackwell <daynajblackwell@gmail.com>
2018-06-16 23:33:24 -0700

    Implemented addNote, removeNote functionality.  Refactored code into utility functions.

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,27 +1,42 @@
-console.log('\nStarting app.js..');
-
+// console.log('\nStarting app.js..');
+// import the file system module
 const fs = require('fs');
+// import the lodash utility library (external dependency)
 const _ = require('lodash');
+// import the yargs optstring parsing library (external dependency)
+// yargs provides an easy to use interface for parsing command-line arguments
 const yargs = require('yargs');
-
+// notes.js contains the function definitions used in this file 
 const notes = require('./notes.js')
-
-const argv = yargs.argv
+// argv will be the yargs formatted argument list
+const argv = yargs.argv;
+// argv._[0] will contain the first argument; the command
 var command = argv._[0];
 // console.log(process.argv);
-// console.log('\nCommand:', command);
 // console.log('Process:', process.argv);
-console.log('Yargs:', argv);
+// console.log('\nCommand:', command);
+// console.log('Yargs:', argv);
 
 if(command === 'add') {
-    // console.log('Adding new note');
-    notes.addNote(argv.title, argv.body)
+// 
+// console.log(argv.yargs);
+var note = notes.addNote(argv.title, argv.body);
+if (note) {
+    console.log("\nNote created");
+    // print title and body using es6 string templates
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+}
+else {console.log("Note title already in use..");}
+
 } else if (command === 'list') {
     notes.getAll();
 } else if (command === 'read') {
     notes.getNote(argv.title);
 }else if (command === 'remove') {
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'No matching title';
+    console.log(message);
 } else {
     console.log('Command not recognized');
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,7 +1,48 @@
-console.log('Starting notes.js..\n');
+// console.log('Starting notes.js..\n');
 
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+    // JSON.parse will construct an object from the string retrieved by the file system module
+        return JSON.parse(notesString);
+    } catch (e) {
+        // catch runs when try fails
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+// tile, body passed in from app.js as argv.title, argv.body
 var addNote = (title, body) => {
-    console.log('Adding note', title,body);
+//  uses es6 shorthand syntax for initializing properties
+//  the property key must match the variable name
+    var notes = fetchNotes();
+    var note = {
+        title, 
+        body   
+    };
+    
+    var duplicateNotes = notes.filter( (note) => (note.title === title) );
+    if(duplicateNotes.length === 0) {
+    // if there are no duplicates
+    // push the note object into the notes array
+        notes.push(note);
+    // use the file system module to synchronously write to notes-data.json; write the string representation of the entire notes array
+        saveNotes(notes);
+        return note;
+    }
+    else {
+        // otherwise there was a duplicate
+        console.log("\nDuplicate entry detected and disgarded..");
+        return false;
+    }
+
+
 };
 
 var getAll = () => {
@@ -10,13 +51,21 @@ var getAll = () => {
 };
 
 var getNote = (title) => {
-    console.log('Getting note with title: '+title);
+    // triggered by 'read' command line argument
+    console.log('Getting note with title: ' + title);
 }
 
 var removeNote = (title) => {
-    console.log('Removing note with title: '+title);
-}
+    var notes = fetchNotes()
+    // create a new array with notes having titles that DO NOT
+    // match the title argument
+    var filteredNotes = notes.filter( (note) => (note.title !== title)  );
+    // save the new array of unique notes
+    saveNotes(filteredNotes);
 
+    return notes.length !== filteredNotes.length;
+    }
+ 
 module.exports = {
     addNote,
     getAll,
