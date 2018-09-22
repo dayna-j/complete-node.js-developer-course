ac0b1f5d6294bf02441b1e38d15932448f204f9b
Dayna <daynajblackwell@gmail.com>
2018-09-21 23:25:38 -0700

    Changed all appropriate 'var' declarations to 'let' declarations.

--- notes-node/app.js
+++ notes-node/app.js
@@ -37,7 +37,7 @@ const argv = yargs.command(
 // .help() displays the usage string and exits the process
 
 // argv._[0] will contain the first argument; the command
-var command = argv._[0];
+let command = argv._[0];
 // console.log(process.argv);
 // console.log('Process:', process.argv);
 // console.log('\nCommand:', command);
@@ -45,7 +45,7 @@ var command = argv._[0];
 
 if(command === 'add') {
 // console.log(argv.yargs);
-var note = notes.addNote(argv.title, argv.body);
+let note = notes.addNote(argv.title, argv.body);
 if (note) {
     // note will be true if a note was returned
     // note will be false if a note was not returned
@@ -56,13 +56,13 @@ if (note) {
 else {console.log("Note title already in use..");}
 
 } else if (command === 'list') {
-    var allNotes = notes.getAll();
+    let allNotes = notes.getAll();
     console.log(`\nPrinting ${allNotes.length} note(s)`);
     allNotes.forEach( (note) => notes.logNote(note));
 
 
 } else if (command === 'read') {
-    var note = notes.getNote(argv.title);
+    let note = notes.getNote(argv.title);
     if (note) {
         console.log('Note found!');
         // print title and body using es6 string templates
@@ -71,10 +71,10 @@ else {console.log("Note title already in use..");}
         console.log('Note not found!');
     }
 }else if (command === 'remove') {
-    var noteRemoved = notes.removeNote(argv.title);
+    let noteRemoved = notes.removeNote(argv.title);
     // if noteRemoved is true, 'Note was removed' is assigned to message
     // if not, 'No matching title' is assigned to message
-    var message = noteRemoved ? 'Note was removed' : 'No matching title';
+    let message = noteRemoved ? 'Note was removed' : 'No matching title';
     console.log(message);
 } else {
     console.log('Command not recognized');
